fdf9ca03d54582674a9b27c1f93c6d97dd1b5c2c
jandrlik <jandrlik@redhat.com>
2023-09-05 15:32:09 +0200

    generic Runner for GHActions

---
+++ genericRunner.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# needs OJDK_VERSION_NUMBER and BOOTJDK_DIR variables set
+
+set -x
+set -e
+set -o pipefail
+
+## assumes that both directories with old and new rpms are provided and filled with relevant rpms
+## this script attempts parallel installation of old and new set of rpms
+
+## resolve folder of this script, following all symlinks,
+## http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+SCRIPT_SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SCRIPT_SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  SCRIPT_DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" && pwd )"
+  SCRIPT_SOURCE="$(readlink "$SCRIPT_SOURCE")"
+  # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+  [[ $SCRIPT_SOURCE != /* ]] && SCRIPT_SOURCE="$SCRIPT_DIR/$SCRIPT_SOURCE"
+done
+readonly SCRIPT_DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" && pwd )"
+
+export RFaT=`bash $SCRIPT_DIR/../get-run-folder-as-tests`
+
+export OJDK_VERSION="ojdk${OJDK_VERSION_NUMBER}"
+
+sh $SCRIPT_DIR/testHeadlessComponents.sh 
