7d5dd7ac9b3265ad154eece58ed08889c8ed6f24
andrlos <80903703+andrlos@users.noreply.github.com>
2023-11-27 19:47:21 +0100

    multiplatforming testsuite (#4)
    ensuring that the testsuite runs on both windows and macos as well

--- testHeadlessComponents.sh
+++ testHeadlessComponents.sh
@@ -18,6 +18,44 @@ while [ -h "$SCRIPT_SOURCE" ]; do # resolve $SOURCE until the file is no longer 
 done
 readonly SCRIPT_DIR="$( cd -P "$( dirname "$SCRIPT_SOURCE" )" && pwd )"
 
+# detecting platform
+platform="$(uname)"
+if [ "$platform" == "Linux" ]; then
+    # Linux-specific code
+    OS="linux"
+    JAVA=$JAVA_HOME/bin/java
+elif [ "$platform" == "Darwin" ]; then
+    # Mac-specific code
+    OS="mac"
+    JAVA=$JAVA_HOME/bin/java
+    #JAVA=java
+elif [ "${platform#"MINGW64_NT"}" != "$platform" ]; then
+    # Windows (Cygwin) specific code
+    OS="windows"
+    JAVA=$JAVA_HOME/bin/java
+else
+    echo "Unsupported platform"
+    exit 1
+fi
+
+function unwrap_file_to_location() {
+  if [ "$OS" == "mac" -o "$OS" == "linux" ]; then
+    tar --strip-components=1 -xf $1 -C $2
+  elif [ "$OS" == "windows" ]; then
+    unzip $1 -d $2
+    # Get the name of the extracted folder (assuming only one folder is present)
+    ls $2
+    extracted_folder_name=$(ls $2)
+
+    # Ensure only one folder is found
+    if [ "$(ls $2 | wc -l)" -eq 1 ]; then
+        # Move the contents to the desired destination without creating a new directory
+        mv "$2/$extracted_folder_name"/* "$2"
+    else
+        echo "Error: More than one directory found in $2."
+    fi
+  fi
+}
 
 function installAlternativeJDK() {
   ARCH=$( uname -m )
@@ -32,21 +70,21 @@ function installAlternativeJDK() {
     BOOTJDK_ARCHIVE_DIR=$WORKSPACE/bootjdkarchive
     mkdir -p $BOOTJDK_ARCHIVE_DIR
     cd $BOOTJDK_ARCHIVE_DIR
-    curl -OLJks "https://api.adoptopenjdk.net/v3/binary/latest/$OJDK_VERSION_NUMBER/ga/linux/$ARCH/jdk/hotspot/normal/adoptopenjdk"
+    curl -OLJks "https://api.adoptopenjdk.net/v3/binary/latest/$OJDK_VERSION_NUMBER/ga/$OS/$ARCH/jdk/hotspot/normal/adoptopenjdk"
     rm -rf ${BOOTJDK_DIR}
     mkdir -p ${BOOTJDK_DIR}
-    tar --strip-components=1 -xf ${BOOTJDK_ARCHIVE_DIR}/*.tar.gz -C ${BOOTJDK_DIR}
+    unwrap_file_to_location ${BOOTJDK_ARCHIVE_DIR}/* ${BOOTJDK_DIR}
   else
     rm -rf ${BOOTJDK_DIR}
     mkdir -p ${BOOTJDK_DIR}
     ls ${BOOTJDK_ARCHIVE_DIR}
-    tar --strip-components=1 -xf ${BOOTJDK_ARCHIVE_DIR}/*${ARCH}.tarxz -C ${BOOTJDK_DIR}
+    unwrap_file_to_location ${BOOTJDK_ARCHIVE_DIR}/*${ARCH}.tarxz ${BOOTJDK_DIR}
   fi
 }
 
 function run_java_with_headless {
   COMPONENTS_TO_TEST=$2
-  java -cp $cp -Djava.awt.headless=$1 MainRunner -test=$COMPONENTS_TO_TEST -jreSdkHeadless=$JRESDK -displayValue=$DISPLAY
+  $JAVA -cp $cp -Djava.awt.headless=$1 MainRunner -test=$COMPONENTS_TO_TEST -jreSdkHeadless=$JRESDK -displayValue=$DISPLAY
 }
 
 function run_swing_component_test_unset {
@@ -88,15 +126,13 @@ function processResults {
 
   if [ $1 -eq 0 ]; then
     let "PASSED+=1"
-    TEST=$(printXmlTest "tps" "$2" "0")
-    BODY+="$TEST
-    " # new line to improve clarity, also is used in TPS/tesultsToJtregs.sh
+    TEST=$(printXmlTest "thc" "$2" "0")
+    BODY+="$TEST"
     echo "$2 PASSED\n"
   else
     let "FAILED+=1"
-    TEST=$(printXmlTest "tps" "$2" "0" "$LOGFILE" "$LOGFILE")
-    BODY+="$TEST
-    " # new line to improve clarity, also is used in TPS/tesultsToJtregs.sh
+    TEST=$(printXmlTest "thc" "$2" "0" "$LOGFILE" "$LOGFILE")
+    BODY+="$TEST"
     echo "$2 FAILED\n"
   fi
 
@@ -137,14 +173,18 @@ LOGFILE=$TMPRESULTS/testHeadlessComponent.log
 installAlternativeJDK
 
 JAVAC_BINARY="${BOOTJDK_DIR}/bin/javac"
+if [ "$OS" == "mac" ]; then
+  JAVAC_BINARY="${BOOTJDK_DIR}/Contents/Home/bin/javac"
+fi
 
 #use bootjdk javac
 #other classes depend on this one, so we might as well just compile the main class
-cp -r $SCRIPT_DIR/testHeadlessComponents/ $WORKSPACE
+cp -r $SCRIPT_DIR/testHeadlessComponents $WORKSPACE
+ls $WORKSPACE
 pushd $WORKSPACE/testHeadlessComponents/jreTestingSwingComponents/src
 
 cp=`mktemp -d`
-$JAVAC_BINARY `find -type f | grep .java` -d $cp
+$JAVAC_BINARY `find . -type f -name "*.java"` -d $cp
 
 declare -A resArray
 set +e
@@ -189,6 +229,9 @@ XMLREPORT=$TMPRESULTS/testHeadlessComponent.jtr.xml
 printXmlHeader $PASSED $FAILED $TESTS $IGNORED "testHeadlessComponent" > $XMLREPORT
 echo "$BODY" >> $XMLREPORT
 printXmlFooter >> $XMLREPORT
+ls -la $XMLREPORT
+
+ls 
 
 for val in ${resArray[@]}; do
   if [[ "$val" -ne "0" ]]; then
